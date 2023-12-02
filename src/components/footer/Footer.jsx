6122fe7912cb72ac45f2f0a1e1fa02d52645cb1f
shashikanth <shashikanthk80@gmail.com>
2023-12-02 23:07:14 +0530

    add basic css and basic folder structure for todo app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.css';
+
+
+
+export default function Footer()
+{
+    return(
+        <footer className={styles.footer}>
+            &copy; 2023 shashikanth
+        </footer>
+    )
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+
+import styles from "./header.module.css";
+export default function Header()
+{
+    return(
+        <header className={styles.header}>
+            <h2>
+                TO-DO APP
+            </h2>
+        </header>
+    )
+}
